233dbd8af2c87e4c8fb86ebf7da7fbe6042d3590
1kivanc <kivanc0556@gmail.com>
2024-08-21 23:12:24 +0300

    sql-zoo

--- SQL-Zoo-Project/quizzes/1-select-from-world.sql
+++ SQL-Zoo-Project/quizzes/1-select-from-world.sql
@@ -19,6 +19,22 @@ FROM world
 WHERE continent = 'South America';
 
 -- 5.Show the name and population for France, Germany, Italy
-SELECT name, population 
-FROM world 
-WHERE name IN ('France', 'Germany', 'Italy');
+SELECT name
+FROM your_table_name
+WHERE name LIKE '%United%';
+
+-- 6.Show the countries which have a name that includes the word 'United'
+SELECT name
+FROM world
+WHERE name LIKE '%United%';
+
+--7.Two ways to be big: A country is big if it has an area of more than 3 million sq km or it has a population of more than 250 million.
+SELECT name, population, area
+FROM world
+WHERE area > 3000000 OR population > 250000000;
+
+-- 8.
+SELECT name, population, area
+FROM world
+WHERE (area > 3000000 AND population <= 250000000)
+   OR (population > 250000000 AND area <= 3000000);
